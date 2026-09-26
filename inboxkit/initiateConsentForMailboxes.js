821f8fbaf16503d9937dfd968f26c86551268e54
Qasim Soomro <qasim@skynur.com>
2026-09-25 19:02:20 -0500

    chore(inboxkit): remove inboxkit app

--- inboxkit/initiateConsentForMailboxes.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description Alias of initiateConsentsForMailboxes (legacy name).
- * @param {Object} input
- * @returns {Object}
- */
-async function initiateConsentForMailboxes(input) {
-  return initiateConsentsForMailboxes(input);
-}
